2937eb6b460984cbc3aba473c3a9bd7a7bf3bd6d
Premsandesh <86162733+Premsandesh@users.noreply.github.com>
2021-10-17 20:04:37 +0530

    Task 11 completed and test case checked

--- Calculator/src/com/calculator/test/StringCalculatorTest.java
+++ Calculator/src/com/calculator/test/StringCalculatorTest.java
@@ -82,4 +82,10 @@ public class StringCalculatorTest {
 		assertEquals(6,StringCalculator.add("//[***]\n1***2***3"));
 	}
 
+	@Test
+	public void test_multiple_delimeters() {
+		assertEquals(6,StringCalculator.add("//[*][%]\n1*2%3"));
+	}
+	
+	
 }
